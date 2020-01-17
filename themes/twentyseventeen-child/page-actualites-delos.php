93f9d1b1b8a4fb2a0021baa75ce3ca1a1a9d0729
Vianney Traina <vianney.traina@edu.devinci.fr>
2020-01-17 16:09:59 +0100

    finish actualites-delos

--- themes/twentyseventeen-child/page-actualites-delos.php
+++ themes/twentyseventeen-child/page-actualites-delos.php
@@ -40,21 +40,216 @@ get_template_part('templates/header', 'jumbotron');
 
 <section class="section-actualites-delos">
     <div class="container-articles-delos">
+
         <article class="article-delos">
             <div class="article-delos-picture">
-                <div class="article-delos-emploi">
+                <div class="article-delos-greeninfo">
                     <p class="article-delos-title">Emploi</p>
                 </div>
             </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Le 17 octobre à eu lieu à Buc une<br>réunion sur l'emploi menée par<br>Monsieur Barrot</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">12 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
         </article>
+
         <article class="article-delos">
-            <div class="article-delos-picture"></div>
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Association</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Les résidents de la Villa du Cèdre<br>participent à l'opération brioches</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">8 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
         </article>
+
         <article class="article-delos">
-            <div class="article-delos-picture"></div>
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Association</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Nos sportifs courent sous<br>les couleurs de Délos</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">7 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformesengagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
         </article>
+
         <article class="article-delos">
-            <div class="article-delos-picture"></div>
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Voyage</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Les résidents de la Villa du Cèdre<br>aux 24h du Mans Camions</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">5 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
         </article>
-    </div>
-</section>
+        
+    </div> <!-- container-articles-delos -->
+    <div class="espacement-container"></div>
+    <div class="container-articles-delos">
+
+        <article class="article-delos">
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Emploi</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Le 17 octobre à eu lieu à Buc une<br>réunion sur l'emploi menée par<br>Monsieur Barrot</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">12 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
+        </article>
+
+        <article class="article-delos">
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Association</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Les résidents de la Villa du Cèdre<br>participent à l'opération brioches</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">8 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
+        </article>
+
+        <article class="article-delos">
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Association</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Nos sportifs courent sous<br>les couleurs de Délos</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">7 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformesengagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
+        </article>
+
+        <article class="article-delos">
+            <div class="article-delos-picture">
+                <div class="article-delos-greeninfo">
+                    <p class="article-delos-title">Voyage</p>
+                </div>
+            </div>
+            <div class="container-articles-delos-informations">
+                <div class="top-delos-informations">
+                    <h4 class="top-title-informations">Les résidents de la Villa du Cèdre<br>aux 24h du Mans Camions</h4>
+                </div>
+                <div class="middle-delos-informations">
+                    <div class="icon-date"></div>
+                    <p class="date-publication">Date de publication:</p>
+                    <p class="gray-date">5 novembre 2019</p>
+                </div>
+                <div class="bottom-delos-informations">
+                    <p class="bottom-informations">Le 17 octobre à eu lieu à Byc une réunion<br>publique sur le hème de l'emploi, les<br> réformes engagées en faveur de l'emploi<br>et de l'apprentissage et leur (...)</p>
+                </div>
+                <div class="articles-delos-button">
+                    <div class="button-green">Lire la suite</div>
+                </div>
+            </div>
+        </article>
+        
+    </div> <!-- container-articles-delos -->
+
+    <div class="container-pages-articles-delos">
+        <div class="container-numero-articles-delos">
+            <p class="first-numero-articles-delos">1</p>
+            <p class="numero-articles-delos">2</p>
+            <p class="numero-articles-delos">3</p>
+            <p class="numero-articles-delos">4</p>
+            <p class="numero-articles-delos">5</p>
+            <p class="numero-articles-delos">Suivant</p>
+        </div>
+    </div> <!-- container-pages-articles-delos -->
+
+</section> <!-- section-actualites-delos -->
+
+<div class="espacement-container"></div>
